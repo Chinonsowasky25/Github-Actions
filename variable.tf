4a7d626b0cdc51bb304d2e64a341278515037d06
Chinonsowasky25 <84383267+Chinonsowasky25@users.noreply.github.com>
2024-09-04 10:07:43 +0100

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,1 +1,23 @@
+provider "google" {
+  project = "sonic-totem-416019"
+  region  = "us-central1"
+}
+
+data "google_client_config" "default" {}
+
+resource "google_container_cluster" "main" {
+  name               = "my-gke-cluster"
+  location           = "us-central1"
+  initial_node_count = 2
+
+  node_config {
+    machine_type = "e2-medium"
+  }
+}
+
+provider "kubernetes" {
+  host                   = google_container_cluster.main.endpoint
+  token                  = data.google_client_config.default.access_token
+  cluster_ca_certificate = base64decode(google_container_cluster.main.master_auth.0.cluster_ca_certificate)
+}
 

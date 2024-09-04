48bfc5187cb6eb4be71db0dca788f8ea1e1b2f02
Chinonsowasky25 <84383267+Chinonsowasky25@users.noreply.github.com>
2024-09-04 10:01:16 +0100

    Create kubernetes.tf

---
+++ kubernetes.tf
@@ -0,0 +1,62 @@
+provider "kubernetes" {
+  host                   = google_container_cluster.main.endpoint
+  token                  = data.google_client_config.default.access_token
+  cluster_ca_certificate = base64decode(google_container_cluster.main.master_auth.0.cluster_ca_certificate)
+}
+
+resource "kubernetes_deployment" "api_deployment" {
+  metadata {
+    name      = "api-deployment"
+    namespace = "default"
+  }
+
+  spec {
+    replicas = 2
+
+    selector {
+      match_labels = {
+        app = "api"
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          app = "api"
+        }
+      }
+
+      spec {
+        container {
+          image = "waskylala/terracicd:latest"
+          name  = "api-container"
+
+          ports {
+            container_port = 80
+          }
+        }
+      }
+    }
+  }
+}
+
+resource "kubernetes_service" "api_service" {
+  metadata {
+    name      = "api-service"
+    namespace = "default"
+  }
+
+  spec {
+    selector = {
+      app = "api"
+    }
+
+    port {
+      protocol    = "TCP"
+      port        = 80
+      target_port = 80
+    }
+
+    type = "LoadBalancer"
+  }
+}
